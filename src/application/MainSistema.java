e9f05ba75092e0c86f9b569352b4ab42f185a1cb
Rodrigo Araujo <rasch.dev@gmail.com>
2024-06-19 14:12:14 -0300

    Started MainSistema

--- src/application/MainSistema.java
+++ src/application/MainSistema.java
@@ -1,11 +1,32 @@
 package application;
 
+import presidiario.*;
+import presidio.*;
+import java.util.Scanner;
+import java.util.Random;
+
 public class MainSistema {
+	static Scanner scan = new Scanner(System.in);
+	static Random ran = new Random();
 
 	public static void main(String[] args) {
-
-  
-
+		boolean run = true;
+		ArvorePS arvPS = new ArvorePS();
+		ArvorePR arvPR = new ArvorePR();
+		
+		do {
+			run = Menu();
+		} while (run);
 	}
-
+	
+	public static Menu() {
+		
+	}
+	
+	public static int codPS() {
+		return ran.nextInt(500001);
+	}
+	public static int codPR() {
+		return ran.nextInt(101);
+	}
 }
